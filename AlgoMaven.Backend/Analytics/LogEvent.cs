d76701bf17104f614a48f7f2c545c273877f98b2
ct <cole2709@hotmail.co.uk>
2024-05-18 19:00:02 +0100

    Event Logging

--- AlgoMaven.Backend/Analytics/LogEvent.cs
+++ AlgoMaven.Backend/Analytics/LogEvent.cs
@@ -3,9 +3,11 @@ namespace AlgoMaven.Backend.Analytics
 {
 	public class LogEvent
 	{
-		public LogEvent()
-		{
-		}
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public string Value { get; set; }
+		public string ID { get; set; }
+		public string Extras { get; set; }
 	}
 }
 
--- AlgoMaven.Backend/Analytics/Logger.cs
+++ AlgoMaven.Backend/Analytics/Logger.cs
@@ -3,8 +3,30 @@ namespace AlgoMaven.Backend.Analytics
 {
 	public class Logger
 	{
-		public Logger()
+		private const string DefaultFileName = "log.csv";
+
+		public void LogEvent(LogEvent logEvent)
 		{
+			if (!File.Exists(DefaultFileName))
+				CreateLogFile();
+
+			using (StreamWriter streamWriter = new StreamWriter(DefaultFileName))
+			{
+				string data = $"{logEvent.Name},{logEvent.ID},{logEvent.Type},{logEvent.Value},{logEvent.Extras}";
+				streamWriter.WriteLine(data);
+			}
+		}
+
+		public void CreateLogFile(string name = DefaultFileName)
+		{
+			try
+			{
+				File.Create(name);
+			}
+			catch
+			{
+				//error handling todo
+			}
 		}
 	}
 }

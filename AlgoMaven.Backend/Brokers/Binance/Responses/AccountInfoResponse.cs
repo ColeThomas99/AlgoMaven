5df385d10f1e88c0a0da29c8fa8a25e6ae052d58
ct <cole2709@hotmail.co.uk>
2024-05-26 16:58:08 +0100

    Added Account Info Response Model

---
+++ AlgoMaven.Backend/Brokers/Binance/Responses/AccountInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace AlgoMaven.Backend.Brokers.Binance.Responses
+{
+	public class AccountInfoResponse
+	{
+        [JsonPropertyName("balances")]
+        public List<Balance> Balances { get; set; }
+        [JsonPropertyName("canTrade")]
+        public bool CanTrade { get; set; }
+	}
+}

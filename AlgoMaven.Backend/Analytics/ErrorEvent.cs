0b417be8fe9c36b08574370effd3b81fd9c092a0
ct <cole2709@hotmail.co.uk>
2024-05-29 11:49:02 +0100

    Moved Codebase to AlgoMaven.Core Namespace

--- AlgoMaven.Backend/Analytics/ErrorEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace AlgoMaven.Backend.Analytics
-{
-	public class ErrorEvent : LogEvent
-	{
-		public string Action { get; set; }
-	}
-}
-
--- AlgoMaven.Backend/MarketData/Binance/Responses/BinanceMarketPriceResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace AlgoMaven.Backend.MarketData.Binance.Responses
-{
-	public class BinanceMarketPriceResponse
-    {
-        public List<BinanceMarketCandle> Candles { get; set; }
-    }
-}
-
